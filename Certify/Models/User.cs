2c2a91bed71331accacf1781acdc01f56e398576
HalushkaYura <yuragalyshka@gmail.com>
2023-06-24 11:50:02 +0300

    UserMidufy

--- Certify/Models/User.cs
+++ Certify/Models/User.cs
@@ -9,7 +9,7 @@ namespace Certify.Models
         public DateTime Birthdate { get; set; }
         [Required,StringLength(100, MinimumLength = 2)]
         public string Firstname { get; set; }
-        [Required,StringLength(100, MinimumLength =2)]
+        [Required, StringLength(100, MinimumLength = 2)]
         public string Lastname { get; set; }
         public ICollection<Signature> Signatures { get; set; }
         public ICollection<Document> Documents { get; set; }
