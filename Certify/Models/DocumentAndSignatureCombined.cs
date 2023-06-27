cf75dc2467a198d179b2fc2232030ac0794bbc75
MVozniak <mikola.v.2016@gmail.com>
2023-06-27 15:06:02 +0300

    Incapsulation changes

--- Certify/Models/DocumentAndSignatureCombined.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Certify.Models
-{
-    public class DocumentAndSignatureCombined
-    {
-        public Document DocumentFC { get; set; }
-        public Signature SignatureFC { get; set; }
-
-    }
-}
---
+++ Certify/ViewModels/DocumentCreate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Certify.ViewModels
+{
+    public class DocumentCreate
+    {
+        public IFormFile UploadedFile { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 1)]
+        public string Title { get; set; }
+
+        [StringLength(150, MinimumLength = 2)]
+        public string? ShortDescription { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
