f3ce60fa775a7d3838e5802deac9afb7adfb716e
MVozniak <mikola.v.2016@gmail.com>
2023-07-05 15:43:49 +0300

    Some changes

--- Certify/ViewModels/Mapping/MappingProfile.cs
+++ Certify/ViewModels/Mapping/MappingProfile.cs
@@ -10,19 +10,13 @@ namespace Certify.ViewModels.Mapping
             this.CreateMap<Document, ForMyDocumentsInfo>()
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Firstname + " " + src.User.Lastname));
             this.CreateMap<ForMyDocumentsInfo, Document>();
-            this.CreateMap<Document, ForMyDocumentsIndex>()
-               .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-               .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-               .ForMember(dest => dest.FileURL, opt => opt.MapFrom(src => src.FileURL));
+            this.CreateMap<Document, ForMyDocumentsIndex>();
             this.CreateMap<ForMyDocumentsCreate, Document>();
            
             this.CreateMap<User, ForMyDocumentsInfo>()
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Firstname + " " + src.Lastname));
 
-            this.CreateMap<Document, ForMyDocumentsEdit>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-                .ForMember(dest => dest.ShortDescription, opt => opt.MapFrom(src => src.ShortDescription));
+            this.CreateMap<Document, ForMyDocumentsEdit>();
 
         }
     }
